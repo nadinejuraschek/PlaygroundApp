e2067f19c01e645783b312b6263bbd263e68d59a
Nadine Ju <nadine.jura@online.de>
2019-12-29 23:25:58 -0600

    post route for playgrounds completed, new playground template form added, new playground can be added

--- app.js
+++ app.js
@@ -9,33 +9,37 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
+var playgroundList = [
+    { name: 'Park One',
+      image: 'https://via.placeholder.com/150'
+    },
+    { name: 'Park Two',
+      image: 'https://via.placeholder.com/150'
+    },
+    { name: 'Park Three',
+      image: 'https://via.placeholder.com/150'
+    }
+];
+
 app.get('/', function(req, res) {
     res.render('landing');
 });
 
 app.get('/playgrounds', function(req, res) {
-    var playgroundList = [
-        { name: 'Park One',
-          image: 'https://via.placeholder.com/150'
-        },
-        { name: 'Park Two',
-          image: 'https://via.placeholder.com/150'
-        },
-        { name: 'Park Three',
-          image: 'https://via.placeholder.com/150'
-        }
-    ];
-
     res.render('playgrounds', { playgrounds: playgroundList });
 });
 
 app.post('/playgrounds', function(req, res) {
     // get data from form
-
+    var newPlayground = { name: req.body.playgroundName, image: req.body.playgroundImg }
     // add to array
     playgroundList.push(newPlayground);
     // redirect to playgrounds
     res.redirect('/playgrounds');
+});
+
+app.get('/playgrounds/new', function(req, res) {
+    res.render('new.ejs');
 });
 
 app.listen(process.env.PORT, function() {
