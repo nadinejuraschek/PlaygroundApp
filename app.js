6e2040d90917a5bd22cdfd25325ff30d548f9a83
Nadine Ju <nadine.jura@online.de>
2020-01-13 00:02:06 -0800

    small refactoring changes

--- app.js
+++ app.js
@@ -15,14 +15,18 @@ app.set('view engine', 'ejs');
 // SCHEMA SETUP
 let playgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 let Playground = mongoose.model('Playground', playgroundSchema);
 
+// CREATE
+
 // Playground.create({ 
 //   name: 'Park Two',
-//   image: 'https://via.placeholder.com/150'
+//   image: 'https://via.placeholder.com/150', 
+//   description: 'Soon to be playground data'
 // }, function(err, playground){
 //   if(err) {
 //      console.log("Error: " + err);
@@ -36,18 +40,19 @@ app.get('/', function(req, res) {
     res.render('landing');
 });
 
-app.get('/playgrounds', function(req, res) {
+app.get('/index', function(req, res) {
     // get all playgrounds from DB
     Playground.find({}, function(err, allPlaygrounds){
       if(err){
         console.log("Error: " + err);
       } else {
-        res.render('playgrounds', { playgrounds: allPlaygrounds });
+        res.render('index', { playgrounds: allPlaygrounds });
       };
     });
 });
 
-app.post('/playgrounds', function(req, res) {
+// NEW - show form to add playground
+app.post('/index', function(req, res) {
     // get data from form
     var newPlayground = { name: req.body.playgroundName, image: req.body.playgroundImg }
     // create new playground and save to DB
@@ -56,13 +61,20 @@ app.post('/playgrounds', function(req, res) {
         console.log('Error: ' + err);
       } else {
         // redirect to playgrounds
-        res.redirect('/playgrounds');
+        res.redirect('/index');
       };
     });
 });
 
 app.get('/playgrounds/new', function(req, res) {
     res.render('new.ejs');
+});
+
+// SHOW - displays more info about a specific playground
+app.get('/playgrounds/:id', function(req, res) {
+    // find playground with provided ID
+    // render show temp with that playground
+    res.render('show');
 });
 
 app.listen(process.env.PORT, function() {
