1e0b19d3c33a77e034ddc63216250bf87b524483
Nadine Ju <nadine.jura@online.de>
2020-02-03 14:09:57 -0800

    major style changes

--- seeds.js
+++ seeds.js
@@ -4,19 +4,28 @@ var Comment   = require("./models/comment");
  
 var data = [
     {
-        name: "Cloud's Rest", 
-        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        name: "Wildflower Meadow Park", 
+        image: "https://images.pexels.com/photos/1134000/pexels-photo-1134000.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        description: "Fenced playground next to the Wildflower Meadow neighborhood. The small park includes three swings (one baby/toddler swing), multiple small play structures, and a slide. Mostly for children age 4 and up.",
+        author: {
+            username: "Peter"
+        }
     },
     {
-        name: "Desert Mesa", 
-        image: "https://farm6.staticflickr.com/5487/11519019346_f66401b6c1.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        name: "Magical Rainbow Park", 
+        image: "https://images.unsplash.com/photo-1555851117-49167a4d285f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80",
+        description: "Our favorite playground! Grand park with lots of monkey bars and climbing equipment, slides and swings for older kids and toddlers, as well as a small sandbox. There are multiple benches for parents to sit nearby. A public restroom is also available.",
+        author: {
+            username: "HappyMama"
+        }
     },
     {
-        name: "Canyon Floor", 
-        image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        name: "Willow Creek Park", 
+        image: "https://images.pexels.com/photos/133458/pexels-photo-133458.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+        description: "Small park with a playstructure. No slides, swings, or sandpits. Good for ages 5+.",
+        author: {
+            username: "Monica"
+        }
     }
 ]
  
@@ -27,36 +36,36 @@ function seedDB(){
             console.log(err);
         }
         console.log("removed playgrounds!");
-//         Comment.remove({}, function(err) {
-//             if(err){
-//                 console.log(err);
-//             }
-//             console.log("removed comments!");
-//              //add a few playgrounds
-//             data.forEach(function(seed){
-//                 Playground.create(seed, function(err, playground){
-//                     if(err){
-//                         console.log(err)
-//                     } else {
-//                         console.log("added a playground");
-//                         //create a comment
-//                         Comment.create(
-//                             {
-//                                 text: "This place is great, but I wish there was internet",
-//                                 author: "Homer"
-//                             }, function(err, comment){
-//                                 if(err){
-//                                     console.log(err);
-//                                 } else {
-//                                     playground.comments.push(comment);
-//                                     playground.save();
-//                                     console.log("Created new comment");
-//                                 }
-//                             });
-//                     }
-//                 });
-//             });
-//         });
+        Comment.remove({}, function(err) {
+            if(err){
+                console.log(err);
+            }
+            console.log("removed comments!");
+             //add a few playgrounds
+            data.forEach(function(seed){
+                Playground.create(seed, function(err, playground){
+                    if(err){
+                        console.log(err)
+                    } else {
+                        console.log("added a playground");
+                        //create a comment
+                        Comment.create(
+                            {
+                                text: "This place is great, but I wish there was internet",
+                                author: "Homer"
+                            }, function(err, comment){
+                                if(err){
+                                    console.log(err);
+                                } else {
+                                    playground.comments.push(comment);
+                                    playground.save();
+                                    console.log("Created new comment");
+                                }
+                            });
+                    }
+                });
+            });
+        });
     }); 
     //add a few comments
 }
