c98f3729b66d0298f382a3706ce94863e7be7245
Nadine Ju <nadine.jura@online.de>
2020-02-07 20:39:18 -0800

    conntect-flash installed and required

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express         = require('express'),
       app             = express(),
       ejs             = require('ejs'),
       mongoose        = require('mongoose'),
+      flash           = require('connect-flash'),
       passport        = require('passport'),
       LocalStrategy   = require('passport-local'),
       methodOverride  = require('method-override'),
@@ -25,6 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
+app.use(flash());
 
 // seedDB();
 
